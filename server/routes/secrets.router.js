0bfa0f773b9808b378f45bb48c9b23b615760ea0
Nicholas Ilacqua <Niservices88@gmail.com>
2021-12-15 20:59:37 -0600

    group work

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware')
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
-
+  let sqlText = `
+  SELECT * FROM "secret"
+  WHERE "secrecy_level" < $1;
+  `
+  let sqlValues = [req.user.clearance_level]
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(sqlText, sqlValues)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
